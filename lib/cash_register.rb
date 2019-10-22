e606f232c9e8f1b3c7979b23fada94717fd6266f
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-10-22 00:26:21 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -28,7 +28,7 @@ class CashRegister
       @to_take_off =  @discount/100.0
       @to_take_off *= @total
       @total -= @to_take_off
-      binding.pry
+      @total = (@total).floor
       return "After the discount, the total comes to $#{total}."
     else
       return "There is no discount to apply."
